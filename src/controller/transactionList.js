a539f832985efd957e1230761b385e3d552fb4a0
Lalu Hilmi <hilmi@prospace-ap.io>
2025-07-09 19:38:15 +0800

    update query

--- src/controller/transactionList.js
+++ src/controller/transactionList.js
@@ -2,7 +2,7 @@ const pool = require('../db');
 
 const getAllTransactions = async () => {
   const result = await pool.query(`
-   SELECT guest.name,
+   SELECT   guest.name,
          rent_transaction.id as transaction_id,
          rent_transaction.booking_code,
          rent_transaction.start_time,
@@ -16,15 +16,16 @@ const getAllTransactions = async () => {
               'name'  , bike.name,
               'status',
                CASE 
-                WHEN rent_transaction_detail.status = 'finished' THEN 'sudah dikembalikan'
-                ELSE 'masih disewa'
+			    WHEN rent_transaction_detail.status = 'ongoing' and rent_transaction.end_time < now()
+				THEN 'waktu habis dan belum dikembalikan'
+				ELSE 'masih disewa'
               END
             )
           ) AS bikes
     FROM rent_transaction
     JOIN guest ON guest.id = rent_transaction.guest_id
     JOIN rent_transaction_detail ON rent_transaction_detail.rent_transaction_id = rent_transaction.id
-    JOIN bike ON bike.id = rent_transaction_detail.bike_id
+    JOIN bike  ON bike.id  = rent_transaction_detail.bike_id
     GROUP BY guest.name,
            rent_transaction.id,
            rent_transaction.booking_code,
